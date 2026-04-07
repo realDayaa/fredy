23647cf2d4454589c4b52cfbe88ed3bd7e4e15bf
Adrian Bach <adrian.bach@gmx.de>
2026-04-07 23:05:42 +0200

    feat: add isihome

---
+++ test/provider/isiHome.test.js
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2026 by Christian Kellner.
+ * Licensed under Apache-2.0 with Commons Clause and Attribution/Naming Clause
+ */
+
+import * as similarityCache from '../../lib/services/similarity-check/similarityCache.js';
+import { get } from '../mocks/mockNotification.js';
+import { mockFredy, providerConfig } from '../utils.js';
+import { expect } from 'vitest';
+import * as provider from '../../lib/provider/isiHome.js';
+
+describe('#isihome testsuite()', () => {
+  it('should test isihome provider', async () => {
+    const Fredy = await mockFredy();
+    provider.init(providerConfig.isihome, [], []);
+
+    const fredy = new Fredy(provider.config, null, null, provider.metaInformation.id, 'isihome', similarityCache);
+    const listing = await fredy.execute();
+
+    if (listing == null || listing.length === 0) {
+      throw new Error('Listings is empty!');
+    }
+
+    expect(listing).toBeInstanceOf(Array);
+    const notificationObj = get();
+    expect(notificationObj).toBeTypeOf('object');
+    expect(notificationObj.serviceName).toBe('isihome');
+    notificationObj.payload.forEach((notify) => {
+      /** check the actual structure **/
+      expect(notify.id).toBeTypeOf('string');
+      expect(notify.price).toBeTypeOf('string');
+      expect(notify.title).toBeTypeOf('string');
+      expect(notify.link).toBeTypeOf('string');
+      expect(notify.address).toBeTypeOf('string');
+      /** check the values if possible **/
+      if (notify.size != null && notify.size.trim().toLowerCase() !== 'k.a.') {
+        expect(notify.size).toContain('m²');
+      }
+      expect(notify.title).not.toBe('');
+      expect(notify.link).toContain('https://isihome.de');
+      expect(notify.address).not.toBe('');
+    });
+  });
+});
